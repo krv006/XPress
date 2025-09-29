e66dcce40dd49a046d4f7c4603a4486097ee8a42
krv006 <rvkamronbek@gmail.com>
2025-09-29 17:10:48 +0500

    blog views full done

--- apps/views/blog_view.py
+++ apps/views/blog_view.py
@@ -1,6 +1,6 @@
 from django.db.models import F
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
@@ -15,24 +15,16 @@ class BlogListAPIView(ListAPIView):
     permission_classes = (AllowAny,)
 
     def get_queryset(self):
-        return BlogPost.objects.order_by('-id')
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        BlogPost.objects.filter(pk=instance.pk).update(views=F('views') + 1)
-        instance.refresh_from_db(fields=['views'])
-        serializer = self.get_serializer(instance, context={"request": request})
-        return Response(serializer.data)
+        queryset = BlogPost.objects.order_by('-id')
+        BlogPost.objects.all().update(views=F('views') + 1)
+        return queryset
 
 
 @extend_schema(tags=["blog"])
-class BlogDetailListAPIView(ListAPIView):
+class BlogDetailAPIView(RetrieveAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogDetailModelSerializer
     permission_classes = (AllowAny,)
-
-    def get_queryset(self):
-        return BlogPost.objects.order_by('-id')
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
