fc61faa9ae75b58cce682c96035bfb9fd4aaa960
krv006 <rvkamronbek@gmail.com>
2025-09-30 22:36:39 +0500

    admin export xlsx

--- apps/admin.py
+++ apps/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from import_export.admin import ImportExportModelAdmin
+from django.contrib.admin import ModelAdmin, TabularInline
+from import_export.admin import ExportMixin
 
 from apps.models import Footer, Stats, Partners, BlogPost, About, FAQ, BlogImage, Category
 from apps.models.main_model import MainPage, QuoteRequest, ChooseXpress
 from apps.resources import MainPageResource
 
 
-class BlogImageInline(admin.TabularInline):
+class BlogImageInline(TabularInline):
     model = BlogImage
     extra = 3
     max_num = 15
 
 
 @admin.register(MainPage)
-class MainPageModelAdmin(ImportExportModelAdmin):
+class MainPageModelAdmin(ExportMixin, ModelAdmin):
     resource_class = MainPageResource
+    list_display = ('id', 'title', 'description')
 
 
 @admin.register(ChooseXpress)
@@ -39,7 +40,7 @@ class StatsModelAdmin(ModelAdmin):
 
 
 @admin.register(Partners)
-class PartnersModelAdmin(ModelAdmin):
+class PartnersModelAdmin(ExportMixin, ModelAdmin):
     pass
 
 
@@ -54,17 +55,17 @@ class FAQModelAdmin(ModelAdmin):
 
 
 @admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
+class BlogPostAdmin(ExportMixin, ModelAdmin):
     inlines = [BlogImageInline]
     list_display = ('id', 'category', 'slug', 'published_at')
     prepopulated_fields = {"slug": ("category",)}  # slug avtomatik
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ExportMixin, ModelAdmin):
     exclude = 'slug',
 
 
 @admin.register(BlogImage)
-class BlogImageAdmin(admin.ModelAdmin):
+class BlogImageAdmin(ModelAdmin):
     list_display = ('id', 'post', 'image')
